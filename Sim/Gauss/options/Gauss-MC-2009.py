ecba0443da63c5ca106140059bc0af3718dd2ffa
Marco Clemencic <marco.clemencic@cern.ch>
2015-03-03 19:34:03 +0100

    v38r3

--- Sim/Gauss/options/Gauss-MC-2009.py
+++ Sim/Gauss/options/Gauss-MC-2009.py
@@ -7,12 +7,12 @@
 ##############################################################################
 
 from Gaudi.Configuration import *
-importOptions("$APPCONFIGOPTS/Gauss/MC09-b3,5TeV-md100-nu1.py")
+importOptions("$APPCONFIGOPTS/Gauss/Beam450GeV-md100-fix1.py")
 
 ##############################################################################
 # latest database tags 
 ##############################################################################
 from Configurables import LHCbApp
 LHCbApp().DDDBtag   = "head-20100119"
-LHCbApp().CondDBtag = "sim-20100202-vc-md100"
+LHCbApp().CondDBtag = "MC-20100205-vc15mm-md100"
 
