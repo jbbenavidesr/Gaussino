3e1fa57f9bec42391e278951d1b6ba913aa286a0
Marco Clemencic <marco.clemencic@cern.ch>
2015-03-03 19:27:14 +0100

    v32r2

--- Sim/Gauss/job/Gauss_Ganga.py
+++ Sim/Gauss/job/Gauss_Ganga.py
@@ -25,7 +25,7 @@
 #------------------------------------------------------------------------------
 # Define the version of Gauss to use
 #------------------------------------------------------------------------------
-GaussVersion = 'v31r2'
+GaussVersion = 'v32r2'
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
@@ -45,7 +45,7 @@ myApplication.version = GaussVersion
 # since ganga takes care of the book-keeping for you :)
 myApplication.optsfile = File ( myApplication.cmt_user_path
    + '/Gauss_' + GaussVersion  + '/Sim/Gauss/' + GaussVersion
-   + '/options/lhcb-200801.opts' )
+   + '/options/lhcb-2008.opts' )
 # Extra options
 # Appended to the end of the main options to override default settings
 #myApplication.extraopts = ''
