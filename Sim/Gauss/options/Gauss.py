eb8ebbc36090ad6ecca56b8fc2fb89cff922f3ec
Marco Clemencic <marco.clemencic@cern.ch>
2015-03-03 19:30:07 +0100

    v37r3

--- Sim/Gauss/options/Gauss.py
+++ Sim/Gauss/options/Gauss.py
@@ -1,46 +1,20 @@
-# Minimal file for running Gauss from python prompt
-#
+##############################################################################
+# File for running Gauss with default options
+##############################################################################
 # Syntax is:
-#   gaudirun.py $GAUSSOPTS/Gauss.py OtherSteerig.{opts,py}
-#
-# To specify an EventType you can give it as argument to follow
-#   gaudirun.py $GAUSSOPTS/Gauss.py $DECFILESROOT/options/30000000.opts
-#
-# Or for a slightly fancier version where the name of the file is automatically
-# set look at the example Gauss-Job.py and use it as
-#   gaudirun.py $GAUSSOPTS/Gauss.py $GAUSSOPTS/Gauss-Job.py
-#
-from Gauss.Configuration import *
+#   gaudirun.py Gauss-Default.py <someInputForTheJob>.py
+##############################################################################
 
-#--Allow to use ns (ie. double) in options of EventClockSvc
-ec = EventClockSvc()
-ec.addTool(FakeEventTime(), name="EventTimeDecoder")
-ec.EventTimeDecoder.StartTime = 0
-ec.EventTimeDecoder.TimeStep  = 25*SystemOfUnits.ns
+from Configurables import Gauss, LHCbApp
 
-#--General Gauss setting and sequence
-importOptions('$GAUSSOPTS/Gauss.opts')
+###############################################################################
+# Set here any steering options.
+# Available steering options and defaults are documented in
+# $GAUSSROOT/python/Gauss/Configuration.py
+###############################################################################
 
-#--Pack events and check consistency
-GaudiSequencer("Simulation").Members += [ PackMCParticle(),
-                                          PackMCVertex() ]
-testMCV = UnpackMCVertex( "TestMCVertex", OutputName= "MC/VerticesTest" )
-testMCP = UnpackMCParticle( "TestMCParticle", OutputName= "MC/ParticlesTest" )
-GaudiSequencer("Simulation").Members += [ testMCV, testMCP,
-                                         CompareMCParticle(),
-                                         CompareMCVertex() ]
+theApp = Gauss()
 
-#--Configuration of output files and 'default' outputs files that can/should
-#--be overwritten in Gauss-Job.py
-spillOverList = ['']
-setOutputs(spillOverList)
-
-#--Geometry dependent options
-LHCbApp( Simulation = True )
-
-#--Geometry to be simulated options
-giGaGeo = GiGaGeo()
-giGaGeo.UseAlignment      = True
-giGaGeo.AlignAllDetectors = True
-importOptions('$GAUSSOPTS/SimVeloGeometry.py')  # -- To misalign VELO
-
+# You MUST also set the tag of the conditions
+#LHCbApp().DDDBtag = <yourChoice>
+#LHCbApp().ConDBtag = <yourChoice>
