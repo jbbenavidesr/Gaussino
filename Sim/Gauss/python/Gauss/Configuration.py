415620a5e35ea36052d143d05ed30695bfa5f503
robbep <robbep>
2016-04-26 13:19:48 +0000

    Add HI beam options
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Gauss/trunk@205403 4525493e-7705-40b1-a816-d608a930855b

--- Sim/Gauss/python/Gauss/Configuration.py
+++ Sim/Gauss/python/Gauss/Configuration.py
@@ -2036,19 +2036,19 @@ class Gauss(LHCbConfigurableUser):
 
         ## Setup EPOS particle type
         gen_t0.MinimumBias.addTool(CRMCProduction,name="CRMCProduction")
-        if ( B1Particle not in self.__ion_pdg__.keys() ):
+        if ( B1Particle not in self.__ion_pdg_id__.keys() ):
             raise RuntimeError( "Unknown particle type: %s"  % B1Particle ) 
-        if ( B2Particle not in self.__ion_pdg__.keys() ):
+        if ( B2Particle not in self.__ion_pdg_id__.keys() ):
             raise RuntimeError( "Unknown particle type: %s"  % B2Particle ) 
-        get_t0.MinimumBias.CRMCProduction.ProjectileID = __ion_pdg__[ B1Particle ] 
-        get_t0.MinimumBias.CRMCProduction.TargetID = __ion_pdg__[ B2Particle ] 
+        get_t0.MinimumBias.CRMCProduction.ProjectileID = self.__ion_pdg_id__[ B1Particle ] 
+        get_t0.MinimumBias.CRMCProduction.TargetID = self.__ion_pdg_id__[ B2Particle ] 
         get_t0.MinimumBias.CRMCProduction.ProjectileMomentum = beamMom / SystemOfUnits.GeV
         get_t0.MinimumBias.CRMCProduction.TargetMomentum = b2Mom / SystemOfUnits.GeV
 
         ## Setup HIJING particle type
         if ( B1Particle != 'p' ):
-            Zproj = str( self.__ion_pdg__[ B1Particle ] )[ 3:5 ]
-            Aproj = int( self.__ion_pdg__[ B1Particle ] )[ 6:8 ]
+            Zproj = str( self.__ion_pdg_id__[ B1Particle ] )[ 3:5 ]
+            Aproj = int( self.__ion_pdg_id__[ B1Particle ] )[ 6:8 ]
             textOptionHijing = "hijinginit izp %s," % Zproj
             textOptionHijing+= "hijinginit iap %s," % Aproj
             textOptionHijing+= "hijinginit proj A,"
@@ -2057,8 +2057,8 @@ class Gauss(LHCbConfigurableUser):
             textOptionHijing+= "hijinginit iap 1,"
             textOptionHijing+= "hijinginit proj P,"
         if ( B2Particle != 'p' ):
-            Ztarg = str( self.__ion_pdg__[ B2Particle ] )[ 3:5 ]
-            Atarg = int( self.__ion_pdg__[ B2Particle ] )[ 6:8 ]
+            Ztarg = str( self.__ion_pdg_id__[ B2Particle ] )[ 3:5 ]
+            Atarg = int( self.__ion_pdg_id__[ B2Particle ] )[ 6:8 ]
             textOptionHijing+= "hijinginit izt %s," % Zproj
             textOptionHijing+= "hijinginit iat %s," % Aproj
             textOptionHijing+= "hijinginit targ A"
