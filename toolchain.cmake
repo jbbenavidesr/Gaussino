0c92075ede6a65fe42a8396beea35d152e756064
gcorti <gcorti>
2016-05-23 14:24:16 +0000

    fix powheg and tmp revert to pythia 186
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Gauss/trunk@206181 4525493e-7705-40b1-a816-d608a930855b

--- toolchain.cmake
+++ toolchain.cmake
@@ -4,18 +4,18 @@ find_file(inherit_heptools_module InheritHEPTools.cmake)
 
 # FIXME: generator versions must be moved to another file
 set(generators_versions
-    alpgen      2.1.4
-    herwig++    2.7.1
-    hijing      1.383bs.2
-    lhapdf      6.1.4
-    photos++    3.56
-    powheg-box  r3043.lhcb
-    pythia6     427.2
-    pythia8     219
-    rivet       2.4.2
-    tauola++    1.1.5
-    thepeg      1.9.2p1
-    yoda        1.5.9
+    alpgen         2.1.4
+    herwig++       2.7.1
+    hijing         1.383bs.2
+    lhapdf         6.1.4
+    photos++       3.56
+    powheg-box-v2  r3043.lhcb
+    pythia6        427.2
+    pythia8        186
+    rivet          2.4.2
+    tauola++       1.1.5
+    thepeg         1.9.2p1
+    yoda           1.5.9
     )
 
 # Process the lines of LCG_generators_*.txt file to extract the
