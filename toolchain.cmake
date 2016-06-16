9a427db92994b0d82fdd75b9d02a2fdf02b2568a
gcorti <gcorti>
2016-06-16 15:56:24 +0000

    change version of pythia8 and add crmc and yoda to cmake
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Gauss/trunk@207062 4525493e-7705-40b1-a816-d608a930855b

--- toolchain.cmake
+++ toolchain.cmake
@@ -11,10 +11,12 @@ set(generators_versions
     photos++       3.56
     powheg-box-v2  r3043.lhcb
     pythia6        427.2
-    pythia8        186
+    pythia8        219
     rivet          2.4.2
-    tauola++       1.1.5
+    tauola++       1.1.6
     thepeg         1.9.2p1
+    crmc           1.5.6
+    yoda           1.5.9
     )
 
 # Process the lines of LCG_generators_*.txt file to extract the
