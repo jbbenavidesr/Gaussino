67bd87b7fdd576adf0e1a4201a466c2481f5d926
Gloria Corti <gloria.corti@cern.ch>
2016-07-01 18:13:17 +0200

    add cmake files for yoda and crmc

---
+++ cmake/FindYODA.cmake
@@ -0,0 +1,33 @@
+# - Try to find YODA
+# Defines:
+#
+#  YODA_FOUND
+#  YODA_HOME (not cached)
+#  YODA_INCLUDE_DIRS (not cached)
+#  YODA_LIBRARY
+#  YODA_LIBRARIES (not cached)
+#  YODA_LIBRARY_DIRS (not cached)
+
+
+find_library(YODA_LIBRARY YODA
+             HINTS $ENV{YODA_ROOT_DIR}/lib)
+
+if(YODA_LIBRARY)
+  get_filename_component(YODA_LIBRARY_DIRS "${YODA_LIBRARY}" DIRECTORY)
+  get_filename_component(YODA_HOME "${YODA_LIBRARY_DIRS}" DIRECTORY)
+  set(YODA_INCLUDE_DIRS "${YODA_HOME}/include")
+  set(YODA_LIBRARIES "${YODA_LIBRARY}")
+  
+endif()
+
+mark_as_advanced(YODA_LIBRARY)
+
+# handle the QUIETLY and REQUIRED arguments and set YODA_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(YODA DEFAULT_MSG YODA_LIBRARY)
+
+mark_as_advanced(YODA_FOUND)
+
+set(YODA_PYTHON_PATH ${YODA_HOME}/lib/python${Python_config_version_twodigit}/site-packages)
+set(YODA_BINARY_PATH ${YODA_HOME}/bin)
