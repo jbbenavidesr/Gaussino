70ad0fce6887231db8370ccdb9f7dd1619bd9794
tevans <tevans>
2016-09-13 15:06:24 +0000

    Add ConvertToSourceCode.cpp
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Gauss/trunk@208049 4525493e-7705-40b1-a816-d608a930855b

---
+++ Gen/AmpGen/apps/ConvertToSourceCode.cpp
@@ -0,0 +1,31 @@
+#include "AmpGen/MinuitParameterSet.h"
+#include "AmpGen/NamedParameter.h"
+#include "AmpGen/FitParameter.h"
+#include "AmpGen/Minimiser.h"
+#include "AmpGen/SumPDF.h"
+#include "AmpGen/FastCoherentSum.h"
+#include "AmpGen/EventType.h"
+#include "AmpGen/EventType.h"
+#include "AmpGen/MintUtilities.h"
+
+using namespace AmpGen;
+
+int main( int argc , char** argv ){
+
+  
+  EventType eventType( 
+    AmpGen::NamedParameter<std::string>("Mother") 
+  , AmpGen::NamedParameter<std::string>("EventType").getVector()  );
+  
+
+  AmpGen::MinuitParameterSet MPS = MPSFromStream();
+
+  FastCoherentSum sig( eventType , MPS , eventType.getEventFormat() ,  "", true);
+
+  SumPDF<std::complex<double>, FastCoherentSum&> pdf( sig ); /// PURE signal pdf
+
+  pdf.setPset( &MPS );
+  pdf.buildLibrary();
+  sig.makeBinary( AmpGen::NamedParameter<std::string>("sourceFile",std::string("test.cpp")) );
+
+}
