69d7d521ceb143e64b01a2d68bc4b69745c9a907
massacrier laure <massacri@lhcb3.lal.in2p3.fr>
2016-09-13 14:02:43 +0200

    Remove lines suggested by Gloria in embedding script EPOSPythia8.py (JIRATICKET-657)

--- Gen/LbCRMC/options/EPOSPythia8.py
+++ Gen/LbCRMC/options/EPOSPythia8.py
@@ -2,7 +2,6 @@ from Gaudi.Configuration import *
 
 from Configurables import Generation
 Generation().CommonVertex = True
-#Generation().EventType = 24142000
 Generation().SampleGenerationTool = "Special"
 from Configurables import Special, DaughtersInLHCbKeepOnlySignal, FixedNInteractions
 Generation().addTool( Special )
@@ -14,13 +13,3 @@ Generation().Special.PileUpProductionTool = "Pythia8Production"
 Generation().Special.ReinitializePileUpGenerator = False
 Generation().Special.addTool( DaughtersInLHCbKeepOnlySignal )
 Generation().Special.DaughtersInLHCbKeepOnlySignal.SignalPID = 443
-from Configurables import ToolSvc
-from Configurables import EvtGenDecay
-ToolSvc().addTool( EvtGenDecay )
-ToolSvc().EvtGenDecay.UserDecayFile = "$DECFILESROOT/dkfiles/incl_Jpsi,mm=NoCut.dec"
-
-def double_int():
-    Generation().addTool( FixedNInteractions )
-    Generation().FixedNInteractions.NInteractions = 2
-
-appendPostConfigAction(double_int)
