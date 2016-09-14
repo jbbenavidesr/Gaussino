e14e1014e3bfe6db76a4f015e9df30aec62f9d64
John J Back <J.J.Back@warwick.ac.uk>
2016-09-14 14:52:57 +0200

    Added TauolaNoSpin keyword in case we want to turn off spin effects in Tauola.

--- Gen/EvtGen/EvtGenModels/EvtTauolaEngine.hh
+++ Gen/EvtGen/EvtGenModels/EvtTauolaEngine.hh
@@ -50,6 +50,7 @@ private:
   bool _initialised;
   int _tauPDG, _nTauolaModes;    
   int _neutPropType, _posPropType, _negPropType;
+  bool _useSpin;
 
   HepMC::GenParticle* createGenParticle(EvtParticle* theParticle);
 
--- Gen/EvtGen/src/EvtTauolaEngine.cpp
+++ Gen/EvtGen/src/EvtTauolaEngine.cpp
@@ -82,6 +82,9 @@ EvtTauolaEngine::EvtTauolaEngine(bool useEvtGenRandom) {
   _posPropType = Tauolapp::TauolaParticle::W_PLUS; // PDG id = 24
   _negPropType = Tauolapp::TauolaParticle::W_MINUS; // PDG id = -24
 
+  // Boolean to specify if we want to use the spin matrices
+  _useSpin = true;
+
   // Set-up possible decay modes _after_ we have read the (user) decay file
   _initialised = false;
 
@@ -323,8 +326,21 @@ void EvtTauolaEngine::setOtherParameters() {
 
       int useOldCurrents = std::atoi(currentString.c_str());
       if (useOldCurrents == 1) {
-	  report(INFO,"EvtGen")<<"Warning: Using old CLEO hadronic currents for TAUOLA"<<endl;
+	  report(INFO,"EvtGen")<<"TAUOLA warning: Using old CLEO hadronic currents"<<endl;
 	  Tauolapp::Tauola::setNewCurrents(0);
+      }
+
+  }
+
+  // 6) TauolaNoSpin: Specify if we want to turn off spin effects, i.e. just generate kinematics
+  std::string spinString = EvtSymTable::get("TauolaNoSpin", iErr);
+
+  if (spinString != "TauolaNoSpin") {
+
+      int noSpinOption = std::atoi(spinString.c_str());
+      if (noSpinOption == 1) {
+	  report(INFO,"EvtGen")<<"TAUOLA warning: disabling spin effects"<<endl;
+	  _useSpin = false;
       }
 
   }
@@ -445,7 +461,7 @@ void EvtTauolaEngine::decayTauEvent(EvtParticle* tauParticle) {
     // For the parent particle, artifically set the PDG to a boson with the same 4-momentum
     // so that spin correlations are calculated inside Tauola. 
     // This leaves the original parent _EvtParticle_ unchanged
-    if (nTaus > 0 && hepMCParent != 0) {
+    if (_useSpin == true && nTaus > 0 && hepMCParent != 0) {
 
 	int parCharge = EvtPDL::chg3(origParentId)/3; // (3*particle charge)/3 = particle charge
 	if (parCharge == 0) {
