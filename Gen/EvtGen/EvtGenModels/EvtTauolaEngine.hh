909a40f2010f4427e1579a78febcbc7859a46a4d
John J Back <J.J.Back@warwick.ac.uk>
2016-09-20 14:03:58 +0200

    Modified EvtTauolaEngine to not change the PDG id of the tau mothers by default. The TauolaNeutralProp and/or TauolaChargedProp definitions have to be explicitly used, if required.

--- Gen/EvtGen/EvtGenModels/EvtTauolaEngine.hh
+++ Gen/EvtGen/EvtGenModels/EvtTauolaEngine.hh
@@ -50,7 +50,6 @@ private:
   bool _initialised;
   int _tauPDG, _nTauolaModes;    
   int _neutPropType, _posPropType, _negPropType;
-  bool _useSpin;
 
   HepMC::GenParticle* createGenParticle(EvtParticle* theParticle);
 
