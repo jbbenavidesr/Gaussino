17b1638cddd77547100facbbb5607e49e50c2766
tevans <tevans>
2016-09-21 10:12:30 +0000

    Moved some Tensor implementation stuff out of header
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Gauss/trunk@208102 4525493e-7705-40b1-a816-d608a930855b

--- Gen/AmpGen/src/Tensor.cpp
+++ Gen/AmpGen/src/Tensor.cpp
@@ -1,4 +1,5 @@
 #include "AmpGen/Tensor.h"
+#include "AmpGen/Utilities.h"
 
 using namespace AmpGen;
 
@@ -307,4 +308,136 @@ Tensor Tensor::Invert() const {
     }
   return data;
 }
+   /// generic contraction operator /// 
+    AmpGen::Tensor Tensor::operator | ( AmpGen::Tensor other ){
+      //    std::vector<std::pair<LorentzIndex, unsigned int> > allIndices; 
+      std::vector<std::pair<unsigned int, unsigned int>> contractions; 
+      //  for( unsigned int i = 0 ; i < m_indices.size(); ++i ) 
+      //      allIndices.push_back( std::make_pair(  m_indices[i]  , m_dim[i])  );
+      std::vector<LorentzIndex> unsummedIndices;
+      for( auto& index : m_indices ) 
+        if( std::find( other.m_indices.begin(), other.m_indices.end(), index ) == other.m_indices.end() ) 
+          unsummedIndices.push_back( index );
+      for( auto& index : other.m_indices )
+        if( std::find( m_indices.begin(), m_indices.end(), index ) == m_indices.end() ) 
+          unsummedIndices.push_back( index );
+
+      for( unsigned int i = 0 ; i < other.m_indices.size(); ++i ){
+        std::vector<LorentzIndex>::iterator it = 
+          std::find( m_indices.begin(), m_indices.end(), other.m_indices[i] );
+        if( it != m_indices.end() ){
+          if( other.m_indices[i].isUpper() != it->isUpper() ){
+            contractions.push_back( std::make_pair(std::distance(m_indices.begin(),it),i) );
+          }
+          else ERROR("Contraction makes no sense!");
+        }
+        // else allIndices.push_back( std::make_pair( other.m_indices[i], other.m_dim[i] ) );
+      }
+      int size = m_dim.size() + other.m_dim.size() - 2*contractions.size();
+      DEBUG(" multiplying " << m_dim.size() << " x "
+          << other.m_dim.size() << " with " 
+          << contractions.size() 
+          << " contractions (dim = " << size << ")" );
+      if(size<0) ERROR("Making an object of negative rank , doesn't make sense");
+      Tensor value( std::vector<unsigned int>(size,4));
+      unsigned nElem = value.nElements();
+      value.m_indices = unsummedIndices; 
+      DEBUG( "Size of object = " << value.nElements() );
+      for( unsigned int elem = 0 ; elem < nElem ; ++elem ){
+        auto coords = value.coords(elem); /// coordinates of this part of the expression /// 
+        std::vector<unsigned int> thisCoordinates( m_dim.size() , 0 ), 
+          otherCoordinates( other.m_dim.size() , 0);
+        unsigned int i = 0 ; 
+        unsigned int j = 0 ; 
+        do {
+          DEBUG("Checking : " << i );
+          if( isIn( contractions, i  , [](
+                  //INFO( "checking : " << i );
+                  const std::pair<unsigned int, unsigned int>& a, 
+                  const unsigned int & b ){ return a.first == b ; } ) ){
+            DEBUG("Skipping coordinate " << i << " as is being contracted " );
+            //  thisCoordinates[i] = 999; 
+            continue;
+          }
+          else {
+            DEBUG(" Setting coordinate " << i << " = " << j  );
+            thisCoordinates[i] = coords[j];
+            j++;
+          }
+          //j++;
+        } while( ++i < m_dim.size() ) ; 
+
+        i=0; 
+        j = m_dim.size() - contractions.size() ;
+        do { 
+          DEBUG("Checking : " << i );
+          if( isIn( contractions, i, [](
+                  const std::pair<unsigned int, unsigned int>& a,
+                  const unsigned int & b ){ return a.second == b ; } ) ){
+            DEBUG("Skipping coordinate " << i << " as is being contracted " );
+
+          }
+          else {
+            otherCoordinates[i]=coords[j];
+            DEBUG( "Setting i = " << i << ", (" << j << ")"<< coords[j] );
+            j++;
+          }
+
+        } while( ++i < other.m_dim.size() );
+        unsigned int sumElements=1;
+        for( unsigned int i=0;i<contractions.size(); ++i) sumElements*=m_dim[i];
+        Expression elementExpression=0;
+        //std::cout << "Element ( " ;
+        //for( unsigned int i = 0 ; i < coords.size() ; ++i ) std::cout << coords[i] << ") = "; 
+        for( unsigned int i = 0 ; i < sumElements; ++i ){  
+          std::vector<unsigned int> contractedCoordinates(contractions.size());
+
+          unsigned int elemI = i;
+          for( unsigned int n = 0 ; n < contractions.size() ; ++n ){
+            unsigned int bi = elemI % 4 ;
+            elemI = (elemI - bi ) /4;
+            contractedCoordinates[n] = bi;
+          }
+          int sign=1;
+          for( unsigned int i = 0 ; i < contractions.size(); ++i ){
+            thisCoordinates[ contractions[i].first ] =   contractedCoordinates[i];
+            otherCoordinates[ contractions[i].second ] = contractedCoordinates[i];
+            sign *= ( contractedCoordinates[i] ) == 3 ? 1 : -1; 
+          }
+          if( sign == 1 ) 
+            elementExpression = elementExpression + get( thisCoordinates ) * other.get( otherCoordinates );  
+          else 
+            elementExpression = elementExpression - get( thisCoordinates ) * other.get( otherCoordinates ); 
+        }
+        value.m_elements[elem] = elementExpression; 
+      }
+      return value; 
+    };
+
+    void Tensor::print() const {
+      INFO( "Dimension of object = " << m_dim.size() << " : ");
+      for( auto& x : m_dim) std::cout << x << ", ";
+      std::cout << std::endl; 
+      if ( m_dim.size() == 0 ) std::cout << m_elements[0].to_string() << std::endl; 
+      else if( m_dim.size() == 1 ){
+        for( unsigned int x=0;x<m_dim[0];++x) 
+          std::cout << m_elements[x].to_string() << std::endl;
+      }
+
+      else if( m_dim.size() == 2 ){
+        for( unsigned int y=0; y < m_dim[1] ; ++y ){
+          for( unsigned int x=0; x < m_dim[0] ; ++x ){
+            std::cout << 
+              m_elements[ index({x,y}) ].to_string() << "     ";
+          }
+          std::cout << std::endl; 
+        }
+      }
+      else {
+        for( unsigned int i = 0 ; i < m_elements.size(); ++i ){
+          INFO( "M(" +vectorToString(coords(i)) + ") = " << m_elements[i].to_string() ); 
+        }   
+
+      }
+    }
 
