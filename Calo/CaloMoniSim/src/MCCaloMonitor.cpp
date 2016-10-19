cbc0beeab80f961a768fdd950b39c3b910021019
Gerhard Raven <gerhard.raven@nikhef.nl>
2016-10-19 16:26:02 +0200

    follow changes in lhcb/LHCb!301

--- Calo/CaloMoniSim/src/MCCaloMonitor.cpp
+++ Calo/CaloMoniSim/src/MCCaloMonitor.cpp
@@ -1,5 +1,3 @@
-// $Id: MCCaloMonitor.cpp,v 1.10 2009-10-23 10:45:22 jonrob Exp $
-
 // Include files
 
 // STL
@@ -9,7 +7,6 @@
 
 // from Gaudi
 #include "Kernel/CaloCellID.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
 // from Event
 #include "Event/MCHit.h"
 #include "Event/MCParticle.h"
@@ -22,7 +19,6 @@
 #include "GaudiAlg/GaudiHistoAlg.h"
 // CaloKernel
 #include "CaloKernel/CaloException.h"
-#include "CaloKernel/CaloCollection.h"
 
 // local
 #include "MCCaloMonitor.h"
