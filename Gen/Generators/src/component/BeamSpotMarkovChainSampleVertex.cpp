cb204dfb71d7cf978268a21f95a5d647f4868044
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2016-11-03 08:48:30 +0000

    Fix typo

--- Gen/Generators/src/component/BeamSpotMarkovChainSampleVertex.cpp
+++ Gen/Generators/src/component/BeamSpotMarkovChainSampleVertex.cpp
@@ -23,7 +23,7 @@ BeamSpotMarkovChainSampleVertex( const std::string& type,
   declareProperty( "Zcut" , m_zcut = 4. ) ; // times SigmaZ
   declareProperty( "BeamParameters" , 
                    m_beamParameters = LHCb::BeamParametersLocation::Default ) ;
-  declareProperty( "NMarkovChainSmaples", m_nMCSamples = 1000 );
+  declareProperty( "NMarkovChainSamples", m_nMCSamples = 1000 );
 }
 
 //=============================================================================
