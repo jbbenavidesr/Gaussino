7f462f44eeff3df40504954c1b836ec868b5f7ec
Marco Clemencic <marco.clemencic@cern.ch>
2016-11-21 17:15:17 +0100

    small update to Pythia8 lookup

--- cmake/FindPythia8.cmake
+++ cmake/FindPythia8.cmake
@@ -13,7 +13,7 @@ find_path(PYTHIA8_INCLUDE_DIR Pythia.h Pythia8/Pythia.h
           HINTS $ENV{PYTHIA8_ROOT_DIR}/include ${PYTHIA8_ROOT_DIR}/include
           PATH_SUFFIXES include)
 
-find_file(PYTHIA8_XML xmldoc PATH_SUFFIXES ../share
+find_file(PYTHIA8_XML xmldoc PATH_SUFFIXES ../share ../share/Pythia8
           HINTS ${PYTHIA8_INCLUDE_DIR}/.. $ENV{PYTHIA8_ROOT_DIR} ${PYTHIA8_ROOT_DIR})
 
 mark_as_advanced(PYTHIA8_INCLUDE_DIR PYTHIA8_XML)
