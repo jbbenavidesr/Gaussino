6a36b830d9f920d7df3b21dbe27b996947c689a7
Gloria Corti <gloria.corti@cern.ch>
2017-06-13 16:56:37 +0000

    Update GetMCRichInfoBase.cpp

--- Sim/GaussRICH/src/SensDet/GetMCRichInfoBase.cpp
+++ Sim/GaussRICH/src/SensDet/GetMCRichInfoBase.cpp
@@ -95,7 +95,7 @@ StatusCode GetMCRichInfoBase::sysExecute(const EventContext& ctx)
   // reset pointers
   m_relationTable = NULL;
   // execute
-  return Rich::AlgBase::sysExecute();
+  return Rich::AlgBase::sysExecute(const EventContext& ctx);
 } 
 
 //=============================================================================
