d6613e59c6baaf293c220bcfb6eb833c45dc4d38
Dmitry Popov <Dmitry.Popov@cern.ch>
2017-06-20 22:55:23 +0200

    Removed deprecated outputLevel() call

--- Sim/GiGa/GiGa/GiGaExtPhysics.h
+++ Sim/GiGa/GiGa/GiGaExtPhysics.h
@@ -58,12 +58,15 @@ public:
       // properties, the extender (if specialized) will pass some specific
       // arguments to the actual constructor.
 
-      int verb = outputLevel();
-      if      (verb == MSG::DEBUG)    { verb = 1 ;} 
-      else if (verb == MSG::VERBOSE)  { verb = 2 ;} 
-      else                            { verb = 0 ;} 
+      // outputLevel() is deprecated
+      // int verb = outputLevel();
+      // if      (verb == MSG::DEBUG)    { verb = 1 ;}
+      // else if (verb == MSG::VERBOSE)  { verb = 2 ;}
+      // else                            { verb = 0 ;}
 
-      m_phys = m_extender.newInstance(name(), verb);
+      //m_phys = m_extender.newInstance(name(), verb);
+      m_phys = m_extender.newInstance(name(), 0);
+
       // m_phys -> SetPhysicsName( name() );
     }
     return m_phys ;
