d9936873e9f56af045c392a2d15a7cff266139ae
Dmitry Popov <Dmitry.Popov@cern.ch>
2017-06-23 14:57:34 +0200

    G4r10 compat fixes for PhysicsLists in Gauss Configurable

--- Sim/Gauss/python/Gauss/Configuration.py
+++ Sim/Gauss/python/Gauss/Configuration.py
@@ -3504,13 +3504,13 @@ class Gauss(LHCbConfigurableUser):
         ## --- Hadron physics:
         if(hadronPhys == "QGSP_BERT"):
             addConstructor("G4HadronElasticPhysics", "ElasticPhysics")
-            addConstructor("HadronPhysicsQGSP_BERT", "QGSP_BERTPhysics")
-            addConstructor("G4QStoppingPhysics", "QStoppingPhysics")
+            addConstructor("G4HadronPhysicsQGSP_BERT", "QGSP_BERTPhysics")
+            # addConstructor("G4QStoppingPhysics", "QStoppingPhysics")
             addConstructor("G4NeutronTrackingCut", "NeutronTrkCut")
         elif(hadronPhys == "QGSP_BERT_HP"):
             addConstructor("G4HadronElasticPhysicsHP", "ElasticPhysicsHP")
-            addConstructor("HadronPhysicsQGSP_BERT_HP", "QGSP_BERT_HPPhysics")
-            addConstructor("G4QStoppingPhysics", "QStoppingPhysics")
+            addConstructor("G4HadronPhysicsQGSP_BERT_HP", "QGSP_BERT_HPPhysics")
+            # addConstructor("G4QStoppingPhysics", "QStoppingPhysics")
             # overwrite the defaut value of the HighPrecision property of the
             # G4HadronElasticPhysics constructor: no longer true, use dedicated
             # constructor
@@ -3518,13 +3518,13 @@ class Gauss(LHCbConfigurableUser):
             #gmpl.ElasticPhysics.OutputLevel = VERBOSE
         elif(hadronPhys == "QGSP_FTFP_BERT"):
             addConstructor("G4HadronElasticPhysics", "ElasticPhysics")
-            addConstructor("HadronPhysicsQGSP_FTFP_BERT", "QGSP_FTFP_BERTPhysics")
-            addConstructor("G4QStoppingPhysics", "QStoppingPhysics")
+            addConstructor("G4HadronPhysicsQGSP_FTFP_BERT", "QGSP_FTFP_BERTPhysics")
+            # addConstructor("G4QStoppingPhysics", "QStoppingPhysics")
             addConstructor("G4NeutronTrackingCut", "NeutronTrkCut")
         elif(hadronPhys == "FTFP_BERT"):
             addConstructor("G4HadronElasticPhysics", "ElasticPhysics")
-            addConstructor("HadronPhysicsFTFP_BERT", "FTFP_BERTPhysics")
-            addConstructor("G4QStoppingPhysics", "QStoppingPhysics")
+            addConstructor("G4HadronPhysicsFTFP_BERT", "FTFP_BERTPhysics")
+            # addConstructor("G4QStoppingPhysics", "QStoppingPhysics")
             addConstructor("G4NeutronTrackingCut", "NeutronTrkCut")
         else:
             raise RuntimeError("Unknown Hadron PhysicsList chosen ('%s')"%hadronPhys)
