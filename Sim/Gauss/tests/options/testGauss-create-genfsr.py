8b3703301ee2d566d2cc68a85a8153d8947ef76f
Davide Fazzini <Davide Fazzini davide.fazzini@cern.ch>
2017-11-09 18:53:46 +0100

    fix bugs in genfsr qmtest

--- Sim/Gauss/tests/options/testGauss-create-genfsr.py
+++ Sim/Gauss/tests/options/testGauss-create-genfsr.py
@@ -5,19 +5,16 @@ GaussGen = GenInit("GaussGen")
 GaussGen.FirstEventNumber = 1
 GaussGen.RunNumber        = 1955
 
-importOptions("$APPCONFIGOPTS/Gauss/Sim08-Beam4000GeV-md100-2012-nu2.5.py")
-
 from Configurables import GaudiSequencer
 seqGenFSR = GaudiSequencer("GenFSRSeq")
-seqGenFSR.Members += [ "GenFSRLog" ]
-
-from Configurables import LHCbApp
-LHCbApp().DDDBtag   = "dddb-20130929-1"
-LHCbApp().CondDBtag = "sim-20130522-1-vc-md100"
+seqGenFSR.Members += ["GenFSRLog"]
+    
+ApplicationMgr().TopAlg += [seqGenFSR]
 
 #--Number of events
 nEvts = 10
 LHCbApp().EvtMax = nEvts
+LHCbApp().Simulation = True
 
 idFile = "genFSR_2012_Gauss_created"
 OutputStream("GaussTape").Output = "DATAFILE='PFN:%s.xgen' TYP='POOL_ROOTTREE' OPT='RECREATE'" %idFile
--- Sim/Gauss/tests/options/testGauss-merge-genfsr.py
+++ Sim/Gauss/tests/options/testGauss-merge-genfsr.py
@@ -4,30 +4,25 @@ def readOnly():
     
     from Configurables import GaudiSequencer
     seqGenFSR = GaudiSequencer("GenFSRSeq")
-    seqGenFSR.Members += [ "GenFSRMerge" ]
-    seqGenFSR.Members += [ "GenFSRLog" ]
+    seqGenFSR.Members += ["GenFSRMerge"]
+    seqGenFSR.Members += ["GenFSRLog"]
 
     ApplicationMgr().TopAlg += [seqGenFSR]
     
     from Configurables import LHCbApp
-    LHCbApp( DataType = "2012", Simulation = True )
+    LHCbApp(DataType = "2012", Simulation = True)
     LHCbApp().DDDBtag   = "dddb-20130929-1"
     LHCbApp().CondDBtag = "sim-20130522-1-vc-md100"
-
-appendPostConfigAction ( readOnly )
-    
-GaudiSequencer("Monitor").Members += [ "GaudiSequencer/GenFSRSeq" ]
-    
-#from PRConfig import TestFileDB
-#TestFileDB.test_file_db["genFSR_2012_sim"].run(configurable=Gauss())
-    
+        
+appendPostConfigAction (readOnly)    
+GaudiSequencer("Monitor").Members += ["GaudiSequencer/GenFSRSeq"]
+        
 EventSelector().Input += [
     "DATAFILE='root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/swtest/lhcb/swtest/genFSR-2012/Gauss-13114005-test1.sim' TYP='POOL_ROOTTREE' OPT='READ'",
     "DATAFILE='root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/swtest/lhcb/swtest/genFSR-2012/Gauss-13114005-test2.sim' TYP='POOL_ROOTTREE' OPT='READ'",
     "DATAFILE='PFN:genFSR_2012_Gauss_created.xgen' TYP='POOL_ROOTTREE' OPT='READ'"
-    #"DATAFILE='/afs/cern.ch/user/d/dfazzini/cmtuser/GaussDev_v48r3/genFSR_2012_Gauss_created.xgen' TYP='POOL_ROOTTREE' OPT='READ'"
     ]
 
 LHCbApp().EvtMax = -1
+LHCbApp().Simulation = True
     
-#IOExtension().inputFiles(['PFN:genFSR_2012_Gauss_merged.sim'])
