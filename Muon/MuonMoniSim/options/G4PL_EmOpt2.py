f227820c09b9ce58cbaf8dc2a8f4f345eb665bbc
Gloria Corti <gloria.corti@cern.ch>
2017-11-14 22:16:27 +0000

    Merge branch 'LHCBGAUSS-874.cleanup' into 'Sim09'
    Removes duplicated files already located elsewhere
    
    See merge request !197

--- Muon/MuonMoniSim/options/G4PL_EmOpt2.py
+++
@@ -1,9 +0,0 @@
-##
-##  File containing options to activate the emstandard_opt2 Electromagnetic physics list
-##  using the FTFP_BERT Hadronic Physics List in Geant4 (the default for production is 
-##  FTFP_BERT and EMNoCuts)
-##
-
-from Configurables import Gauss
-
-Gauss().PhysicsList = {"Em":'Opt2', "Hadron":'FTFP_BERT', "GeneralPhys":True, "LHCbPhys":True}
--- Muon/MuonMoniSim/scripts/muonmonisim.sh
+++ Muon/MuonMoniSim/scripts/muonmonisim.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-python $MUONMONISIMROOT/scripts/runmuontest.py EmStd
 python $MUONMONISIMROOT/scripts/runmuontest.py EmOpt1
-python $MUONMONISIMROOT/scripts/runmuontest.py EmOpt2
-python $MUONMONISIMROOT/scripts/runmuontest.py EmOpt3
 python $MUONMONISIMROOT/scripts/runmuontest.py EmNoCuts
-python $MUONMONISIMROOT/scripts/runmuontest.py EmLHCb
-python $MUONMONISIMROOT/scripts/runmuontest.py EmLHCbNoCuts
-
+python $MUONMONISIMROOT/scripts/runmuontest.py EmNoCutsNoLHCb
