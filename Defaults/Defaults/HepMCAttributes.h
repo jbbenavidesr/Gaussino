48afbc7a3e5dfbd3350890b3a74b381f5c2a5030
Dominik Muller <dominik.muller@cern.ch>
2018-03-26 17:59:11 +0200

    Forgot something important

---
+++ Defaults/Defaults/HepMCAttributes.h
@@ -0,0 +1,10 @@
+#pragma once
+namespace Gaussino {
+  namespace HepMC {
+    namespace Attributes{
+      const std::string GeneratorName = "GeneratorName";
+      const std::string SignalProcessID = "SignalProcessID";
+      const std::string SignalProcessVertex = "SignalProcessVertex";
+    }
+  }
+}
