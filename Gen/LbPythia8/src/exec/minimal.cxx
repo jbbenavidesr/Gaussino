07370f256e3d88ff3301bb331103ff12146a1a96
Dominik Muller <dominik.muller@cern.ch>
2018-09-03 13:33:15 +0200

    Integrated comparison directly to minimal Pythia8 reproducibility test

--- Gen/LbPythia8/src/exec/minimal.cxx
+++ Gen/LbPythia8/src/exec/minimal.cxx
@@ -4,16 +4,31 @@
 #include "HepMC/GenParticle.h"
 #include "HepMC/GenVertex.h"
 #include "HepMC/WriterAscii.h"
-#include "LbPythia8/Pythia8ProductionMT.h"
+#include "HepMCUtils/CompareGenEvent.h"
 #include "Pythia8/Basics.h"
 #include "Pythia8/Pythia.h"
 #include "Pythia8HepMC/Pythia8ToHepMC3.h"
+#include <map>
 #include <thread>
+
+typedef std::map<int, HepMC::GenEvent*> STORE;
+
+class RndForPythia : public Pythia8::RndmEngine
+{
+public:
+  RndForPythia( CLHEP::HepRandomEngine& engine ) : m_gen( engine, 0, 1 ) {}
+  virtual double flat()
+  {
+    auto val = m_gen();
+    return val;
+  }
+
+private:
+  CLHEP::RandFlat m_gen;
+};
 
 HepMC::GenEvent* convert( Pythia8::Pythia* pythia )
 {
-  // Convert to HepMC.
-
   auto theEvent = new HepMC::GenEvent{};
   HepMC::Pythia8ToHepMC3 conversion;
   conversion.set_print_inconsistency( false );
@@ -24,43 +39,39 @@ HepMC::GenEvent* convert( Pythia8::Pythia* pythia )
 
 HepMC::GenEvent* run_pythia( int seed, std::string dir, Pythia8::Pythia* pythia )
 {
-
   std::ofstream file;
   std::stringstream outfilename, buffer;
   outfilename << dir << "/" << seed << ".txt";
-  file.open( outfilename.str() );
   CLHEP::MixMaxRng engine;
   engine.setSeed( seed );
 
-  // If passing a buffer here, the calls to the random engine are
-  // monitored and backtraces printed to the file.
-  //RndForPythia rnd_pythia{engine, &buffer};
   RndForPythia rnd_pythia{engine};
   pythia->setRndmEnginePtr( &rnd_pythia );
   pythia->next();
-  auto evt = convert(pythia);
-  file << buffer.str();
-  file.close();
+  auto evt = convert( pythia );
+  // HepMC::WriterAscii writer( outfilename.str() );
+  // writer.write_event( *evt );
   return evt;
 }
 
-void run_seq(std::string name, const std::vector<int> seeds)
+void run_seq( std::string name, const std::vector<int> seeds, STORE* store )
 {
-  Pythia8::Pythia pythia{"/cvmfs/sft.cern.ch/lcg/releases/LCG_93/MCGenerators/pythia8/230/x86_64-centos7-gcc7-opt/share/Pythia8/xmldoc",
-                                false};
+  Pythia8::Pythia pythia{
+      "/cvmfs/sft.cern.ch/lcg/releases/LCG_93/MCGenerators/pythia8/235/x86_64-centos7-gcc7-opt/share/Pythia8/xmldoc",
+      false};
 
-  pythia.readString("Beams:frameType = 3" );
-  pythia.readString("Beams:idA = 2212" );
-  pythia.readString("Beams:idB = 2212" );
-  //pythia.readString("SoftQCD:all = on" );
-  //pythia.readString("SoftQCD:all = off");
-  //pythia.readString("SoftQCD:nonDiffractive = on");
-  //pythia.readString("SoftQCD:elastic = on");
-  pythia.readString("SoftQCD:singleDiffractive = on"); // DOOOO
-  //pythia.readString("SoftQCD:doubleDiffractive = on"); // DOOOO
-  //pythia.readString("SoftQCD:centralDiffractive = on");
-  //pythia.readString("SoftQCD:inelastic = on"); // DOOOO
-  //pythia.readString("Beams:allowMomentumSpread = on" );
+  pythia.readString( "Beams:frameType = 3" );
+  pythia.readString( "Beams:idA = 2212" );
+  pythia.readString( "Beams:idB = 2212" );
+  // pythia.readString("SoftQCD:all = on" );
+  // pythia.readString("SoftQCD:all = off");
+  // pythia.readString("SoftQCD:nonDiffractive = on");
+  // pythia.readString("SoftQCD:elastic = on");
+  pythia.readString( "SoftQCD:singleDiffractive = on" ); // DOOOO
+  // pythia.readString("SoftQCD:doubleDiffractive = on"); // DOOOO
+  // pythia.readString("SoftQCD:centralDiffractive = on");
+  // pythia.readString("SoftQCD:inelastic = on"); // DOOOO
+  pythia.readString( "Beams:allowMomentumSpread = on" );
 
   // Create a random engine to be used during init()
   CLHEP::MixMaxRng engine;
@@ -68,31 +79,40 @@ void run_seq(std::string name, const std::vector<int> seeds)
   RndForPythia rnd_pythia1{engine};
   pythia.setRndmEnginePtr( &rnd_pythia1 );
   pythia.init();
-  HepMC::WriterAscii writer(name + ".txt");
   for ( auto s1 : seeds ) {
-    std::cout << "Doing " << s1 << " from " << name <<std::endl;
-    auto evt = run_pythia( s1, name, &pythia );
-    // Set an attribute to compare events with the same seed later
-    evt->add_attribute("GaudiEventNumber", std::make_shared<HepMC::IntAttribute>(s1));
-    writer.write_event(*evt);
+    auto evt       = run_pythia( s1, name, &pythia );
+    ( *store )[s1] = evt;
   }
 }
 
 int main( int, char** )
 {
-  std::vector <int> seeds1, seeds2;
-  for(int i=1; i<=200; i++){
-    seeds1.push_back(i);
-    seeds2.push_back(i);
+  std::vector<int> seeds1, seeds2, tmp;
+  for ( int i = 1; i <= 200; i++ ) {
+    seeds1.push_back( i );
   }
-  std::random_shuffle(std::begin(seeds2), std::end(seeds2));
-  // Event sequence
-  //std::thread t1{run_seq, "default1", seeds1};
-  //std::thread t2{run_seq, "default2", seeds2};
-  //t1.join();
-  //t2.join();
+  for ( int i = 1; i <= 200; i++ ) {
+    seeds2.push_back( i );
+  }
+  std::random_shuffle( std::begin( seeds2 ), std::end( seeds2 ) );
 
-  run_seq("sequential1", seeds1);
-  run_seq("sequential2", seeds2);
+  STORE store1, store2;
+
+  // Event sequence
+  std::thread t1{run_seq, "sequential1", seeds1, &store1};
+  std::thread t2{run_seq, "sequential2", seeds2, &store2};
+  t1.join();
+  t2.join();
+
+  for ( auto& val : store1 ) {
+    auto evt1 = val.second;
+    auto evt2 = store2[val.first];
+    if ( !HepMC::compareGenEvent( *evt1, *evt2 ) ) {
+      std::cerr << "Event with seed " << val.first << " not equal" << std::endl;
+      return 1;
+    }
+  }
+  std::cerr << "Everything good!" << std::endl;
+
   return 0;
 }
