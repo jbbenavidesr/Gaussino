4da0cf242014ea33b9641431730bbde9a4f510d4
Dominik Muller <dominik.muller@cern.ch>
2019-07-08 13:02:31 +0200

    Added always to gigamessage

--- Sim/GiGaMTFactories/GiGaMTFactories/GiGaTool.h
+++ Sim/GiGaMTFactories/GiGaMTFactories/GiGaTool.h
@@ -27,6 +27,7 @@ public:
   void verbose( std::string message ) const override { msg << MSG::VERBOSE << message << endmsg; }
   void error( std::string message ) const override { msg << MSG::ERROR << message << endmsg; }
   void info( std::string message ) const override { msg << MSG::INFO << message << endmsg; }
+  void always( std::string message ) const override { msg << MSG::ALWAYS << message << endmsg; }
   void warning( std::string message ) const override { msg << message << endmsg; }
   int level() const override { return msg.level(); }
 
