a9d0b5f9af8bd124d8ce81b3609c04a7133aff4d
Dominik Muller <dominik.muller@cern.ch>
2019-08-22 15:19:49 +0200

    HepMC2 for compatible with GenHeaders ...

--- toolchain.cmake
+++ toolchain.cmake
@@ -136,5 +136,3 @@ endif()
 if(CMAKE_PREFIX_PATH AND CMAKE_VERSION VERSION_GREATER "3.6.0")
   list(FILTER CMAKE_PREFIX_PATH EXCLUDE REGEX "(LCG_|lcg/nightlies).*Geant4")
 endif()
-
-set(CMAKE_PREFIX_PATH /cvmfs/sft.cern.ch/lcg/releases/LCG_${heptools_version}/hepmc3/3.0.0/${BINARY_TAGCOMP} ${CMAKE_PREFIX_PATH})                                                                                                                            
