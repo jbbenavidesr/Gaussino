995eb0273a1ba05b234836038e3e821a2712f349
Dominik Muller <dominik.muller@cern.ch>
2019-09-04 17:31:25 +0200

    Allow for SensDet -> volume pairs to be specified in the configuration

--- Sim/GiGaMTFactories/src/det/GiGaMTDetectorConstructionFAC.cpp
+++ Sim/GiGaMTFactories/src/det/GiGaMTDetectorConstructionFAC.cpp
@@ -7,29 +7,54 @@
 DECLARE_COMPONENT( GiGaMTDetectorConstructionFAC )
 DECLARE_COMPONENT( GiGaMTProxyDetectorConstructionFAC )
 
-G4VUserDetectorConstruction* GiGaMTDetectorConstructionFAC::construct() const
-{
+StatusCode GiGaMTDetectorConstructionFAC::initialize() {
+  auto sc = extends::initialize();
+  // Retrieve the factory tools here to avoid the retrieval happening in multiple
+  // threads
+  for ( auto& keypairs : m_sens_dets ) { keypairs.second.retrieve(); }
+  return sc;
+}
+G4VUserDetectorConstruction* GiGaMTDetectorConstructionFAC::construct() const {
   auto detconst = new GiGaMTDetectorConstruction();
   detconst->SetWorld( m_geoSvc->constructWorld() );
 
   return detconst;
 }
 
-G4VUserDetectorConstruction* GiGaMTProxyDetectorConstructionFAC::construct() const
-{
+G4VUserDetectorConstruction* GiGaMTProxyDetectorConstructionFAC::construct() const {
   auto detconst = new GiGaMTProxyDetectorConstruction();
   detconst->SetWorldConstructor( [&]() {
     debug() << "Calling world constructor" << endmsg;
     auto world = m_geoSvc->constructWorld();
-    for ( auto& tool : m_afterGeo ) {
-      tool->process();
-    }
+    for ( auto& tool : m_afterGeo ) { tool->process(); }
     return world;
   } );
   detconst->SetSDConstructor( [&]() {
     debug() << "Calling SD and Field constructor" << endmsg;
     m_geoSvc->constructSDandField();
+    DressVolumes();
   } );
 
   return detconst;
+}
+
+#include "Geant4/G4LogicalVolumeStore.hh"
+#include "Geant4/G4SDManager.hh"
+
+void GiGaMTDetectorConstructionFAC::DressVolumes() const {
+  auto sdmanager = G4SDManager::GetSDMpointer();
+  for ( auto& [name, volumes] : m_namemap ) {
+    auto& tool    = m_sens_dets.at( name );
+    auto  sensdet = tool->construct();
+    sdmanager->AddNewDetector( sensdet );
+    for ( auto& volname : volumes ) {
+      auto vol = G4LogicalVolumeStore::GetInstance()->GetVolume( volname );
+      if ( vol ) {
+        debug() << "Attaching " << name << " to " << volname << endmsg;
+        vol->SetSensitiveDetector( sensdet );
+      } else {
+        error() << "Couldn't find " << volname << endmsg;
+      }
+    }
+  }
 }
--- Sim/GiGaMTFactories/src/det/GiGaMTDetectorConstructionFAC.h
+++ Sim/GiGaMTFactories/src/det/GiGaMTDetectorConstructionFAC.h
@@ -12,20 +12,36 @@ class IGaussinoTool;
 // Factory class implemented as a GaudiTool that creates and configures the
 // GiGaMTRunMangager singleton.
 
+
 class GiGaMTDetectorConstructionFAC : public extends<GiGaTool, GiGaFactoryBase<G4VUserDetectorConstruction>>
 {
 public:
   using extends::extends;
 
   G4VUserDetectorConstruction* construct() const override;
+  StatusCode initialize() override;
 
 protected:
+  typedef std::pair<std::string, std::vector<std::string>> SensDetNameVolumesPair;
+  typedef std::map<std::string, std::vector<std::string>> SensDetNameVolumesMap;
+  typedef ToolHandle<GiGaFactoryBase<G4VSensitiveDetector>> SensDetFac;
+  typedef std::map<std::string, SensDetFac> SensDetVolumeMap;
+
+  void DressVolumes() const;
   ServiceHandle<IGiGaMTGeoSvc> m_geoSvc{this, "GiGaMTGeoSvc", "GiGaMTGeo"};
   ToolHandleArray<IGaussinoTool> m_afterGeo{this};
   Gaudi::Property<std::vector<std::string>> m_afterGeoNames{
       this, "AfterGeoConstructionTools", {},
       tool_array_setter(m_afterGeo, m_afterGeoNames),
       Gaudi::Details::Property::ImmediatelyInvokeHandler{true}};
+private:
+  SensDetVolumeMap m_sens_dets;
+  Gaudi::Property<SensDetNameVolumesMap> m_namemap{this, "SensDetVolumeMap", {},[this]( Gaudi::Details::PropertyBase& ){
+    for(auto & keypairs: this->m_namemap){
+      auto & name = keypairs.first;
+      m_sens_dets.emplace( std::piecewise_construct, std::forward_as_tuple( name ), std::forward_as_tuple( name, this ) );
+    }
+  }, Gaudi::Details::Property::ImmediatelyInvokeHandler{true}};
 };
 
 class GiGaMTProxyDetectorConstructionFAC : public GiGaMTDetectorConstructionFAC
