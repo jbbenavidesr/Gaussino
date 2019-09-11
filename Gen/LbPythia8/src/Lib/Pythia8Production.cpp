c6e340d46c5edf8d76e5637fc71fb2f0d0c4e417
Dominik Muller <dominik.muller@cern.ch>
2019-09-11 11:34:15 +0200

    Fix unit conversion for non MT queue

--- Gen/LbPythia8/src/Lib/Pythia8Production.cpp
+++ Gen/LbPythia8/src/Lib/Pythia8Production.cpp
@@ -368,8 +368,12 @@ StatusCode Pythia8Production::toHepMC(HepMC3::GenEvent* theEvent,
   // Convert to HepMC.
   HepMC3::Pythia8ToHepMC3 conversion;
   conversion.set_print_inconsistency(m_validate_HEPEVT);
+  auto old_momentum_unit = theEvent->momentum_unit();
+  auto old_length_unit = theEvent->length_unit();
+  conversion.set_print_inconsistency(m_validate_HEPEVT);
   if (!(conversion.fill_next_event(*m_pythia, theEvent))) 
-    return Error("Failed to convert Pythia 8 event to HepMC.");
+    return Error("Failed to convert Pythia 8 event to HepMC3.");
+  theEvent->set_units(old_momentum_unit, old_length_unit);
   // Convert status codes and IDs.
   for ( auto& p : theEvent->particles() ) {
     int status = p->status();
