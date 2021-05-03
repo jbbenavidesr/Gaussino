07a46fc8432a65eafa78a90b04b00260a501bbf2
Michal Mazurek <michal.mazurek@cern.ch>
2021-05-03 14:36:07 +0200

    Add Repository configuration

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,11 @@ release = 'v0r1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +52,22 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Global file metadata
+html_context = {
+    'display_gitlab': True,
+    'gitlab_host': 'gitlab.cern.ch',
+    'gitlab_user': 'Gaussino',
+    'gitlab_repo': 'Gaussino',
+    'gitlab_version': 'master/docs/',
+}
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
