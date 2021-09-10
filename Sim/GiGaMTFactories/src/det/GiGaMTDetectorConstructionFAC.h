ae62d0827c6f65878241d0b3eb4718b60812d7bf
Michal Mazurek <michal.mazurek@cern.ch>
2021-09-10 15:06:34 +0200

    Apply formatting in GiGaMTDetectorConstructionFAC

--- Sim/GiGaMTFactories/src/det/GiGaMTDetectorConstructionFAC.h
+++ Sim/GiGaMTFactories/src/det/GiGaMTDetectorConstructionFAC.h
@@ -22,38 +22,44 @@ class IGaussinoTool;
 // Factory class implemented as a GaudiTool that creates and configures the
 // GiGaMTRunMangager singleton.
 
-
-class GiGaMTDetectorConstructionFAC : public extends<GiGaTool, GiGaFactoryBase<G4VUserDetectorConstruction>>
-{
+class GiGaMTDetectorConstructionFAC : public extends<GiGaTool, GiGaFactoryBase<G4VUserDetectorConstruction>> {
 public:
   using extends::extends;
 
   G4VUserDetectorConstruction* construct() const override;
-  StatusCode initialize() override;
+  StatusCode                   initialize() override;
 
 protected:
-  typedef std::pair<std::string, std::vector<std::string>> SensDetNameVolumesPair;
-  typedef std::map<std::string, std::vector<std::string>> SensDetNameVolumesMap;
+  typedef std::pair<std::string, std::vector<std::string>>  SensDetNameVolumesPair;
+  typedef std::map<std::string, std::vector<std::string>>   SensDetNameVolumesMap;
   typedef ToolHandle<GiGaFactoryBase<G4VSensitiveDetector>> SensDetFac;
-  typedef std::map<std::string, SensDetFac> SensDetVolumeMap;
+  typedef std::map<std::string, SensDetFac>                 SensDetVolumeMap;
 
-  void DressVolumes() const;
-  void SaveGDML(G4LogicalVolume*) const;
-  ServiceHandle<IGiGaMTGeoSvc> m_geoSvc{this, "GiGaMTGeoSvc", "GiGaMTGeo"};
-  ToolHandleArray<IGaussinoTool> m_afterGeo{this};
-  Gaudi::Property<std::vector<std::string>> m_afterGeoNames{
-      this, "AfterGeoConstructionTools", {},
-      tool_array_setter(m_afterGeo, m_afterGeoNames),
-      Gaudi::Details::Property::ImmediatelyInvokeHandler{true}};
+  void                                      DressVolumes() const;
+  void                                      SaveGDML( G4LogicalVolume* ) const;
+  ServiceHandle<IGiGaMTGeoSvc>              m_geoSvc{this, "GiGaMTGeoSvc", "GiGaMTGeo"};
+  ToolHandleArray<IGaussinoTool>            m_afterGeo{this};
+  Gaudi::Property<std::vector<std::string>> m_afterGeoNames{this,
+                                                            "AfterGeoConstructionTools",
+                                                            {},
+                                                            tool_array_setter( m_afterGeo, m_afterGeoNames ),
+                                                            Gaudi::Details::Property::ImmediatelyInvokeHandler{true}};
 
   Gaudi::Property<std::string> m_schema{this, "Schema", "$GDML_base/src/GDMLSchema/gdml.xsd"};
   Gaudi::Property<std::string> m_outfile{this, "Output", ""};
+
 private:
-  SensDetVolumeMap m_sens_dets;
-  Gaudi::Property<SensDetNameVolumesMap> m_namemap{this, "SensDetVolumeMap", {},[this]( Gaudi::Details::PropertyBase& ){
-    for(auto & keypairs: this->m_namemap){
-      auto & name = keypairs.first;
-      m_sens_dets.emplace( std::piecewise_construct, std::forward_as_tuple( name ), std::forward_as_tuple( name, this ) );
-    }
-  }, Gaudi::Details::Property::ImmediatelyInvokeHandler{true}};
+  SensDetVolumeMap                       m_sens_dets;
+  Gaudi::Property<SensDetNameVolumesMap> m_namemap{this,
+                                                   "SensDetVolumeMap",
+                                                   {},
+                                                   [this]( Gaudi::Details::PropertyBase& ) {
+                                                     for ( auto& keypairs : this->m_namemap ) {
+                                                       auto& name = keypairs.first;
+                                                       m_sens_dets.emplace( std::piecewise_construct,
+                                                                            std::forward_as_tuple( name ),
+                                                                            std::forward_as_tuple( name, this ) );
+                                                     }
+                                                   },
+                                                   Gaudi::Details::Property::ImmediatelyInvokeHandler{true}};
 };
