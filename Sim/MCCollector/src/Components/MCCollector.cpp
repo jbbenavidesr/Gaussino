2fe58da56e5699bfa82a7561a9af62d94692d9df
Michal Mazurek <michal.mazurek@cern.ch>
2023-01-31 14:46:45 +0100

    Port generic infrastructure of CaloCollector to Gaussino

---
+++ Sim/MCCollector/src/Components/MCCollector.cpp
@@ -0,0 +1,32 @@
+/*****************************************************************************\
+* (c) Copyright 2023 CERN for the benefit of the LHCb and FCC Collaborations  *
+*                                                                             *
+* This software is distributed under the terms of the Apache License          *
+* version 2 (Apache-2.0), copied verbatim in the file "COPYING".              *
+*                                                                             *
+* In applying this licence, CERN does not waive the privileges and immunities *
+* granted to it by virtue of its status as an Intergovernmental Organization  *
+* or submit itself to any jurisdiction.                                       *
+\*****************************************************************************/
+// Gaudi
+#include "GaudiAlg/Consumer.h"
+// MCCollector
+#include "MCCollector/MCCollectorTuple.h"
+
+namespace MCCollector {
+  using TupleBaseClass = Gaudi::Functional::Traits::BaseClass_t<TupleAlg>;
+  class Collector
+      : public Gaudi::Functional::Consumer<void( const LHCb::MCHeader&, const LHCb::MCHits& ), TupleBaseClass> {
+
+  public:
+    Collector( const std::string& name, ISvcLocator* pSvcLocator )
+        : Consumer( name, pSvcLocator,
+                    {KeyValue{"MCHeader", LHCb::MCHeaderLocation::Default}, KeyValue{"CollectorHits", ""}} ){};
+
+    void operator()( const LHCb::MCHeader& evt, const LHCb::MCHits& collHits ) const override {
+      fillCollectorTuple( evt, collHits );
+    }
+  };
+} // namespace MCCollector
+
+DECLARE_COMPONENT_WITH_ID( MCCollector::Collector, "MCCollector" )
---
+++ Sim/MCCollector/tests/options/external_planes.py
@@ -0,0 +1,60 @@
+###############################################################################
+# (c) Copyright 2022 CERN for the benefit of the LHCb and FCC Collaborations  #
+#                                                                             #
+# This software is distributed under the terms of the Apache License          #
+# version 2 (Apache-2.0), copied verbatim in the file "COPYING".              #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
+
+from Gaudi.Configuration import DEBUG
+from GaudiKernel import SystemOfUnits as units
+from Configurables import ExternalDetectorEmbedder
+
+# number of planes to generate
+PLANES_NO = 4
+
+# adding external detectors
+external = ExternalDetectorEmbedder("ExternalDetectorEmbedder_0")
+# generating planes
+shapes = {}
+sensitive = {}
+hit = {}
+moni = {}
+for plane in range(PLANES_NO):
+    shapes['Plane{}'.format(plane + 1)] = {
+        "Type": "Cuboid",
+        "xPos": 0. * units.m,
+        "yPos": 0. * units.m,
+        "zPos": (1. + plane) * units.m,
+        "xSize": 10. * units.m,
+        "ySize": 10. * units.m,
+        "zSize": .01 * units.mm,
+        "OutputLevel": DEBUG,
+    }
+    sensitive['Plane{}'.format(plane + 1)] = {
+        "Type": "MCCollectorSensDet",
+        "RequireEDep": False,
+        "PrintStats": True,
+        "OutputLevel": DEBUG,
+    }
+    hit['Plane{}'.format(plane + 1)] = {
+        "Type": "GetMCCollectorHitsAlg",
+        "OutputLevel": DEBUG,
+    }
+    moni['Plane{}'.format(plane + 1)] = {
+        "Type": "MCCollector",
+        "HitsPropertyName": "CollectorHits",
+        "OutputLevel": DEBUG,
+    }
+external.Shapes = shapes
+external.Sensitive = sensitive
+external.Hit = hit
+external.Moni = moni
+
+from Configurables import NTupleSvc
+NTupleSvc().Output = [
+    "FILE1 DATAFILE='CaloCollector.root' TYP='ROOT' OPT='NEW'"
+]
