839c4a4554f0a7425cea9e6ccb93b63ad2a25158
Sebastien Ponce <sebastien.ponce@cern.ch>
2025-04-17 15:26:22 +0200

    Modernized GenMonitorAlg and dropped usage of GaudiHistoAlg

--- Moni/GaussMonitor/src/Components/GenMonitorAlg.cpp
+++ Moni/GaussMonitor/src/Components/GenMonitorAlg.cpp
@@ -8,58 +8,95 @@
 * granted to it by virtue of its status as an Intergovernmental Organization  *
 * or submit itself to any jurisdiction.                                       *
 \*****************************************************************************/
-// $Id: GenMonitorAlg.cpp,v 1.16 2010-02-24 19:02:33 robbep Exp $
-// Include files
 
-// from Gaudi
-#include "GaudiKernel/SystemOfUnits.h"
-
-// From HepMC
-#include "HepMC3/GenEvent.h"
-#include "HepMC3/GenParticle.h"
-#include "HepMC3/GenVertex.h"
+#include "GaussGenUtil.h"
 
 // From LHCb
 #include "Kernel/ParticleID.h"
 
-// local
-#include "GaussGenUtil.h"
-#include "GenMonitorAlg.h"
-
-// Get the default names for the HepMC attributes
 #include "Defaults/HepMCAttributes.h"
+#include "Defaults/Locations.h"
 
-//-----------------------------------------------------------------------------
-// Implementation file for class : GenMonitorAlg
-//
-// 2003-11-13 : Patrick Robbe
-// 2005-04-11 : G.Corti
-// 2018-03-08 : D. Muller
-//-----------------------------------------------------------------------------
+#include "Gaudi/Accumulators/StaticHistogram.h"
+#include "GaudiAlg/Consumer.h"
+#include "GaudiKernel/SystemOfUnits.h"
+
+#include "HepMC3/GenEvent.h"
+#include "HepMC3/GenParticle.h"
+#include "HepMC3/GenVertex.h"
+#include "HepMCUser/typedefs.h"
+
+#include <atomic>
+#include <mutex>
+#include <string>
+
+namespace Acc = Gaudi::Accumulators;
+
+/**
+ *  Monitoring algorithms for the generator sequences
+ *
+ *  @author Patrick Robbe (modified G.Corti)
+ *  @date   2005-04-11
+ *
+ *  Modifications for function framework and HepMC3
+ *
+ *  @author Dominik Muller
+ *  @date   2018-03-08
+ */
+class GenMonitorAlg : public Gaudi::Functional::Consumer<void( const HepMC3::GenEventPtrs& )> {
+public:
+  GenMonitorAlg( const std::string& name, ISvcLocator* pSvcLocator )
+      : Consumer( name, pSvcLocator, { KeyValue{ "Input", Gaussino::HepMCEventLocation::Default } } ) {}
+
+  void               operator()( const HepMC3::GenEventPtrs& ) const override;
+  virtual StatusCode finalize() override;
+
+private:
+  Gaudi::Property<bool> m_produceHistos{ this, "HistoProduce", false, "Switch on/off the production of histograms" };
+
+  mutable std::atomic<int> m_counter{ 0 };
+  mutable std::atomic<int> m_counterstable{ 0 };
+  mutable std::atomic<int> m_counterCharged{ 0 };
+  mutable std::atomic<int> m_counterChInEta{ 0 };
+  mutable std::atomic<int> m_nEvents{ 0 };
+
+  mutable Acc::StaticHistogram<1> m_hNPart{ this, "1", "Multiplicity all particles", { 300, 0., 2999. } };
+  mutable Acc::StaticHistogram<1> m_hNStable{ this, "2", "Multiplicity protostable particles", { 300, 0., 2999. } };
+  mutable Acc::StaticHistogram<1> m_hNSCharg{
+      this, "3", "Multiplicity stable charged particles", { 300, -0.5, 299.5 } };
+  mutable Acc::StaticHistogram<1> m_hNSChEta{
+      this, "4", "Multiplicity stable charged particles in LHCb eta", { 300, -0.5, 299.5 } };
+  mutable Acc::StaticHistogram<1> m_hProcess{ this, "5", "Process type", { 5101, -0.5, 5100.5 } };
+  mutable Acc::StaticHistogram<1> m_hNPileUp{ this, "10", "Num. of primary interaction per bunch", { 11, -0.5, 100.5 } };
+  mutable Acc::StaticHistogram<1> m_hPrimX{ this, "11", "PrimaryVertex x (mm)", { 200, -1.0, 1.0 } };
+  mutable Acc::StaticHistogram<1> m_hPrimY{ this, "12", "PrimaryVertex y (mm)", { 200, -1.0, 1.0 } };
+  mutable Acc::StaticHistogram<1> m_hPrimZ{ this, "13", "PrimaryVertex z (mm)", { 100, -200., 200. } };
+  mutable Acc::StaticHistogram<1> m_hPrimZV{ this, "14", "PrimaryVertex z, all Velo (mm)", { 100, -1000., 1000. } };
+  mutable Acc::StaticHistogram<1> m_hPrimZE{
+      this, "15", "PrimaryVertex z, all Exp Area (mm)", { 105, -1000., 20000. } };
+  mutable Acc::StaticHistogram<1> m_hPrimT{ this, "16", "PrimaryVertex t (ns)", { 151, -75.5, 75.5 } };
+  mutable Acc::StaticHistogram<1> m_hPartP{ this, "21", "Momentum of all particles (GeV)", { 100, 0., 100. } };
+  mutable Acc::StaticHistogram<1> m_hPartPDG{ this, "22", "PDGid of all particles", { 10000, -4999., 5000. } };
+  mutable Acc::StaticHistogram<1> m_hProtoP{ this, "31", "Momentum of protostable particles (GeV)", { 100, 0., 100. } };
+  mutable Acc::StaticHistogram<1> m_hProtoPDG{ this, "32", "PDGid of protostable particles", { 10000, -4999., 5000. } };
+  mutable Acc::StaticHistogram<1> m_hProtoLTime{ this, "33", "Lifetime protostable particles (mm)", { 105, -1., 20. } };
+  mutable Acc::StaticHistogram<1> m_hStableEta{
+      this, "44", "Pseudorapidity stable charged particles", { 150, -15., 15. } };
+  mutable Acc::StaticHistogram<1> m_hStablePt{ this, "45", "Pt stable charged particles", { 100, 0., 20. } };
+  mutable Acc::StaticHistogram<2> m_hPrimXvsZ{
+      this, "17", "PrimaryVertex x vs z (mm)", { 100, -500., 500. }, { 100, -1., 1. } };
+  mutable Acc::StaticHistogram<2> m_hPrimYvsZ{
+      this, "18", "PrimaryVertex y vs z (mm)", { 100, -500., 500. }, { 100, -1., 1. } };
+
+  Gaudi::Property<double>      m_minEta{ this, "MinEta", 2.0 };
+  Gaudi::Property<double>      m_maxEta{ this, "MaxEta", 4.9 };
+  Gaudi::Property<std::string> m_generatorName{ this, "ApplyTo", "" };
+};
 
 // Declaration of the Algorithm Factory
 DECLARE_COMPONENT( GenMonitorAlg )
 
-StatusCode GenMonitorAlg::initialize() {
-
-  StatusCode sc = GaudiHistoAlg::initialize(); // must be executed first
-  if ( sc.isFailure() ) return sc;             // error printed already by GaudiHistoAlg
-
-  debug() << "==> Initialize" << endmsg;
-
-  if ( m_generatorName.empty() ) {
-    info() << "Monitor will be applied to events produced with generator " << m_generatorName << endmsg;
-  }
-
-  if ( produceHistos() ) { bookHistos(); }
-
-  return StatusCode::SUCCESS;
-}
-
 void GenMonitorAlg::operator()( const HepMC3::GenEventPtrs& hepmcevents ) const {
-  std::lock_guard<std::mutex> lock( m_histo_lock );
-  debug() << "==> Execute" << endmsg;
-
   // Initialize counters
   int nParticles( 0 ), nParticlesStable( 0 );
   int nParticlesStableCharged( 0 ), nParChStabEtaAcc( 0 );
@@ -76,12 +113,12 @@ void GenMonitorAlg::operator()( const HepMC3::GenEventPtrs& hepmcevents ) const 
     debug() << "Monitor for " << gen_name << endmsg;
 
     // Get the signal process ID from the attributes
-    if ( produceHistos() ) {
+    if ( m_produceHistos.value() ) {
       auto sig_proc_id_attr =
           hepmcevent->attribute<HepMC3::IntAttribute>( Gaussino::HepMC::Attributes::SignalProcessID );
       if ( sig_proc_id_attr ) {
         auto sig_proc_id = sig_proc_id_attr->value();
-        m_hProcess->fill( sig_proc_id );
+        ++m_hProcess[sig_proc_id];
       }
     }
 
@@ -89,28 +126,28 @@ void GenMonitorAlg::operator()( const HepMC3::GenEventPtrs& hepmcevents ) const 
     nPileUp++;
     for ( auto& hepMCpart : hepmcevent->particles() ) {
       nParticles++;
-      if ( produceHistos() ) {
+      if ( m_produceHistos.value() ) {
         // Identify primary vertex and fill histograms
         if ( !primFound ) {
           if ( ( hepMCpart->status() == 1 ) || ( hepMCpart->status() == 888 ) ) {
             primFound = true;
             if ( hepMCpart->production_vertex() ) {
-              m_hPrimX->fill( hepMCpart->production_vertex()->position().x() / Gaudi::Units::mm );
-              m_hPrimY->fill( hepMCpart->production_vertex()->position().y() / Gaudi::Units::mm );
-              m_hPrimZ->fill( hepMCpart->production_vertex()->position().z() / Gaudi::Units::mm );
-              m_hPrimZV->fill( hepMCpart->production_vertex()->position().z() / Gaudi::Units::mm );
-              m_hPrimZE->fill( hepMCpart->production_vertex()->position().z() / Gaudi::Units::mm );
-              m_hPrimT->fill( hepMCpart->production_vertex()->position().t() / Gaudi::Units::ns );
-              m_hPrimXvsZ->fill( hepMCpart->production_vertex()->position().z() / Gaudi::Units::mm,
-                                 hepMCpart->production_vertex()->position().x() / Gaudi::Units::mm );
-              m_hPrimYvsZ->fill( hepMCpart->production_vertex()->position().z() / Gaudi::Units::mm,
-                                 hepMCpart->production_vertex()->position().y() / Gaudi::Units::mm );
+              ++m_hPrimX[hepMCpart->production_vertex()->position().x() / Gaudi::Units::mm];
+              ++m_hPrimY[hepMCpart->production_vertex()->position().y() / Gaudi::Units::mm];
+              ++m_hPrimZ[hepMCpart->production_vertex()->position().z() / Gaudi::Units::mm];
+              ++m_hPrimZV[hepMCpart->production_vertex()->position().z() / Gaudi::Units::mm];
+              ++m_hPrimZE[hepMCpart->production_vertex()->position().z() / Gaudi::Units::mm];
+              ++m_hPrimT[hepMCpart->production_vertex()->position().t() / Gaudi::Units::ns];
+              ++m_hPrimXvsZ[{ hepMCpart->production_vertex()->position().z() / Gaudi::Units::mm,
+                              hepMCpart->production_vertex()->position().x() / Gaudi::Units::mm }];
+              ++m_hPrimYvsZ[{ hepMCpart->production_vertex()->position().z() / Gaudi::Units::mm,
+                              hepMCpart->production_vertex()->position().y() / Gaudi::Units::mm }];
             }
           }
         }
 
-        m_hPartP->fill( hepMCpart->momentum().p3mod() / Gaudi::Units::GeV );
-        m_hPartPDG->fill( hepMCpart->pdg_id() );
+        ++m_hPartP[hepMCpart->momentum().p3mod() / Gaudi::Units::GeV];
+        ++m_hPartPDG[hepMCpart->pdg_id()];
       }
       // Note that the following is really multiplicity of particle defined
       // as stable by Pythia just after hadronization: all particles known by
@@ -118,10 +155,10 @@ void GenMonitorAlg::operator()( const HepMC3::GenEventPtrs& hepmcevents ) const 
       // and gamma all togheter as stable ...
       if ( ( hepMCpart->status() != 2 ) && ( hepMCpart->status() != 3 ) ) {
         nParticlesStable++;
-        if ( produceHistos() ) {
-          m_hProtoP->fill( hepMCpart->momentum().p3mod() / Gaudi::Units::GeV );
-          m_hProtoPDG->fill( hepMCpart->pdg_id() );
-          m_hProtoLTime->fill( GaussGenUtil::lifetime( hepMCpart ) / Gaudi::Units::mm );
+        if ( m_produceHistos.value() ) {
+          ++m_hProtoP[hepMCpart->momentum().p3mod() / Gaudi::Units::GeV];
+          ++m_hProtoPDG[hepMCpart->pdg_id()];
+          ++m_hProtoLTime[GaussGenUtil::lifetime( hepMCpart ) / Gaudi::Units::mm];
         }
         // Charged stable particles meaning really stable after EvtGen
         LHCb::ParticleID pID( hepMCpart->pdg_id() );
@@ -134,9 +171,9 @@ void GenMonitorAlg::operator()( const HepMC3::GenEventPtrs& hepmcevents ) const 
             double pseudoRap = hepMCpart->momentum().pseudoRapidity();
             // in LHCb acceptance
             if ( ( pseudoRap > m_minEta ) && ( pseudoRap < m_maxEta ) ) { ++nParChStabEtaAcc; }
-            if ( produceHistos() ) {
-              m_hStableEta->fill( pseudoRap );
-              m_hStablePt->fill( hepMCpart->momentum().perp() / Gaudi::Units::GeV );
+            if ( m_produceHistos.value() ) {
+              ++m_hStableEta[pseudoRap];
+              ++m_hStablePt[hepMCpart->momentum().perp() / Gaudi::Units::GeV];
             }
           }
         }
@@ -144,29 +181,21 @@ void GenMonitorAlg::operator()( const HepMC3::GenEventPtrs& hepmcevents ) const 
     }
   }
 
-  if ( produceHistos() ) {
-    m_hNPart->fill( nParticles );
-    m_hNStable->fill( nParticlesStable );
-    m_hNSCharg->fill( nParticlesStableCharged );
-    m_hNSChEta->fill( nParChStabEtaAcc );
-    m_hNPileUp->fill( nPileUp );
+  if ( m_produceHistos.value() ) {
+    ++m_hNPart[nParticles];
+    ++m_hNStable[nParticlesStable];
+    ++m_hNSCharg[nParticlesStableCharged];
+    ++m_hNSChEta[nParChStabEtaAcc];
+    ++m_hNPileUp[nPileUp];
   }
   m_counter += nParticles;
   m_counterstable += nParticlesStable;
   m_counterCharged += nParticlesStableCharged;
   m_counterChInEta += nParChStabEtaAcc;
   m_nEvents++;
-
-  debug() << "Event number " << m_nEvents << " contains " << nParticles << " particles" << endmsg;
 }
 
-//=============================================================================
-//  Finalize
-//=============================================================================
 StatusCode GenMonitorAlg::finalize() {
-
-  debug() << "==> Finalize" << endmsg;
-
   info() << std::endl
          << "======================== Generators Statistics ====================" << std::endl
          << "=                                                                 =" << std::endl
@@ -187,38 +216,5 @@ StatusCode GenMonitorAlg::finalize() {
          << "= Mean charged stable multiplicity in LHCb eta: " << m_counterChInEta / (double)m_nEvents << std::endl
          << "=                                                                 =" << std::endl
          << "===================================================================" << endmsg;
-
-  return GaudiHistoAlg::finalize();
-}
-
-//============================================================================
-// Booking of histograms
-//============================================================================
-void GenMonitorAlg::bookHistos() {
-
-  debug() << "==> Book histograms" << endmsg;
-
-  m_hNPart      = book( 1, "Multiplicity all particles", 0., 2999., 300 );
-  m_hNStable    = book( 2, "Multiplicity protostable particles", 0., 2999., 300 );
-  m_hNSCharg    = book( 3, "Multiplicity stable charged particles", -0.5, 299.5, 300 );
-  m_hNSChEta    = book( 4, "Multiplicity stable charged particles in LHCb eta", -0.5, 299.5, 300 );
-  m_hProcess    = book( 5, "Process type", -0.5, 5100.5, 5101 );
-  m_hNPileUp    = book( 10, "Num. of primary interaction per bunch", -0.5, 100.5, 101 );
-  m_hPrimX      = book( 11, "PrimaryVertex x (mm)", -1.0, 1.0, 200 );
-  m_hPrimY      = book( 12, "PrimaryVertex y (mm)", -1.0, 1.0, 200 );
-  m_hPrimZ      = book( 13, "PrimaryVertex z (mm)", -200., 200., 100 );
-  m_hPrimZV     = book( 14, "PrimaryVertex z, all Velo (mm)", -1000., 1000., 100 );
-  m_hPrimZE     = book( 15, "PrimaryVertex z, all Exp Area (mm)", -1000., 20000., 105 );
-  m_hPrimT      = book( 16, "PrimaryVertex t (ns)", -75.5, 75.5, 151 );
-  m_hPrimXvsZ   = book2D( 17, "PrimaryVertex x vs z (mm)", -500., 500., 100, -1., 1., 100 );
-  m_hPrimYvsZ   = book2D( 18, "PrimaryVertex y vs z (mm)", -500., 500., 100, -1., 1., 100 );
-  m_hPartP      = book( 21, "Momentum of all particles (GeV)", 0., 100., 100 );
-  m_hPartPDG    = book( 22, "PDGid of all particles", -4999., 5000., 10000 );
-  m_hProtoP     = book( 31, "Momentum of protostable particles (GeV)", 0., 100., 100 );
-  m_hProtoPDG   = book( 32, "PDGid of protostable particles", -4999., 5000., 10000 );
-  m_hProtoLTime = book( 33, "Lifetime protostable particles (mm) ", -1., 20., 105 );
-  m_hStableEta  = book( 44, "Pseudorapidity stable charged particles", -15., 15., 150 );
-  m_hStablePt   = book( 45, "Pt stable charged particles", 0., 20., 100 );
-
-  return;
+  return Consumer::finalize();
 }
