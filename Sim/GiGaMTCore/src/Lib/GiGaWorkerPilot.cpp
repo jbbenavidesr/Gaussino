ef4753af707a7b3b731e7083bc0d5588d0cf5053
Witek Pokorski <witoldp@joulertx.dyndns.cern.ch>
2025-07-02 17:33:58 +0200

    removing leftover printout

--- Sim/GiGaMTCore/src/Lib/GiGaWorkerPilot.cpp
+++ Sim/GiGaMTCore/src/Lib/GiGaWorkerPilot.cpp
@@ -174,8 +174,6 @@ void GiGaWorkerPilot::operator()() {
 
     mgr->ProcessEvent( evt );
 
-    std::cout << "GiGaWorkerPilot processed event " << evt->GetEventID() << std::endl;
-
     if ( m_postprocessing ) {
       if ( evt->ToBeKept() ) {
         debug( "Asked to keep this event by Geant4" );
