6d153d82bec2836a94917df4d103fcb349bada6f
Gitlab CI <noreply@cern.ch>
2025-08-08 08:37:48 +0000

    pre-commit fixes
    patch generated by https://gitlab.cern.ch/Gaussino/Gaussino/-/jobs/59955267

--- Sim/EDM/include/EDM/SimHeader.h
+++ Sim/EDM/include/EDM/SimHeader.h
@@ -43,7 +43,7 @@ namespace Gsino {
     void setEvtNumber( long long value ) { m_evtNumber = value; }
 
   private:
-    unsigned int m_runNumber = 0;  ///< Run number
+    unsigned int m_runNumber = 0; ///< Run number
     long long    m_evtNumber = 0; ///< Event number
 
   }; // class SimHeader
